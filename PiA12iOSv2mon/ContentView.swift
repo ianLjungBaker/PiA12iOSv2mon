6f8a5d09a7264c9071b2b5f1f9d35f6c2886a6b6
Ian Ljung-Baker <iancbaker01@gmail.com>
2023-11-06 09:28:43 +0100

    changed again

--- PiA12iOSv2mon/ContentView.swift
+++ PiA12iOSv2mon/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello,Hello world!")
+            Text("Hello,Hello,Hello world!")
         }
         .padding()
     }
