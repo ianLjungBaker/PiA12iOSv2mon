498ffaad00560701178b1942190da9eb6ef9c504
Ian Ljung-Baker <iancbaker01@gmail.com>
2023-11-06 09:09:39 +0100

    Initial Commit

---
+++ PiA12iOSv2mon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PiA12iOSv2mon
+//
+//  Created by ian Ljung-Baker on 2023-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PiA12iOSv2mon/PiA12iOSv2monApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiA12iOSv2monApp.swift
+//  PiA12iOSv2mon
+//
+//  Created by ian Ljung-Baker on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct PiA12iOSv2monApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
